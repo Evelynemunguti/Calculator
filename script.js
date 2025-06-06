0a40c1c24886c365bc061cef5bc7d6df06b1bce3
Evelyne Munguti <evelynekyamwai@gmail.com>
2025-06-07 02:47:18 +0300

    added a click event listener for each button

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ const row= document.getElementsByClassName("row");
 const calc= document.getElementById("row");
 const display= document.getElementById("display");
 
-let currentInput= "";
+let currentInput= '';
+
+buttton.forEach(button => {
+
+    button.addEventListener('click',() =>{
+        const value= getAttribute('data-value');
+    })
+    
+});
+
